3b5bf5b51d69adae89ecc4406aa88bdd2b9bd0c1
pattahemanthkumar25csm-eng <pattahemanthkumar.25.csm@anits.edu.in>
2026-08-23 16:25:22 +0530

    Update Inherit1.java

--- assignments/assignment1/Inherit1.java
+++ assignments/assignment1/Inherit1.java
@@ -1,24 +1,32 @@
-
-class f{
+// it is single inheritance program 
+class f{           // create a class 
     int a =10;
     void sum(){
-        System.out.println("sum is " + (a+34)); // single inheritance is executed
+        System.out.println("sum is " + (a+34)); 
+in class f have initialise variable and one method 
     }
 }
+// create another class 
+// class f is inherited by class S to using of keyword " extends"
+/* here class f is super class
+and class S is sub class*/
 class s extends f{
     int b = 54;
-    f add  = new f();
+   // f add  = new f();
     void sub(){
         System.out.println( " subtraction is " + (b-10) );
     }
 
 }
-public class Inherit1 {
+public class Inherit1 {    // main method 
     public static void main(String[] args) {
-
+// create an object for sub class 
         s oc = new s();
+// print and call super class variable 
         System.out.println(" variable can access is" + (oc.a) );
-        System.out.println(" can access" + (oc.b));
+// print the sub class variable 
+        System.out.println(" variable can access" + (oc.b));
+// call super class methods from sub class object 
         oc.sum();
         oc.sub();
         
